617be95c0f3f871bc0297da5a7ae2df681798134
Andrew A. Chuev <andrew.chuev@gmail.com>
2024-10-26 19:48:14 +0300

    Update commit console command

--- app/Console/Commands/GitCommitCommand.php
+++ app/Console/Commands/GitCommitCommand.php
@@ -29,19 +29,36 @@ class GitCommitCommand extends Command
     {
         $message = $this->argument('message');
 
+        // Get database configuration from .env
+        $dbHost = config('database.connections.mysql.host');
+        $dbUser = config('database.connections.mysql.username');
+        $dbPassword = config('database.connections.mysql.password');
+        $dbName = config('database.connections.mysql.database');
+
         // Run mysqldump to create a database backup
-        $dumpCommand = 'mysqldump -h 192.168.88.60 -u homestead -psecret chill_booking > chill_booking_backup.sql';
+        $dumpCommand = "mysqldump -h $dbHost -u $dbUser -p$dbPassword $dbName > {$dbName}_backup.sql";
         $dumpResult = shell_exec($dumpCommand);
+        if ($dumpResult === null) {
+            $this->error('Failed to create database backup. Please check your configuration and try again.');
+            return 1;
+        }
         $this->info('Database backup created.');
 
         // Run git add -A
         $addResult = shell_exec('git add -A');
+        if ($addResult === null) {
+            $this->error('Failed to add changes to git.');
+            return 1;
+        }
         $this->info($addResult);
 
         // Run git commit -m
         $commitResult = shell_exec("git commit -m \"$message\"");
+        if ($commitResult === null) {
+            $this->error('Failed to commit changes.');
+            return 1;
+        }
         $this->info($commitResult);
 
         return 0;
-    }
-}
+    }}
