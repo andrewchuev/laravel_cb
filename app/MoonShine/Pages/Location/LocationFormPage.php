121a146c2640b215a3724c5deb33174efeb41c09
Andrew A. Chuev <andrew.chuev@gmail.com>
2024-10-29 22:49:22 +0200

    Update location layout

--- app/MoonShine/Pages/Location/LocationFormPage.php
+++ app/MoonShine/Pages/Location/LocationFormPage.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Pages\Location;
 
+use MoonShine\Fields\Text;
 use MoonShine\Pages\Crud\FormPage;
 use MoonShine\Components\MoonShineComponent;
 use MoonShine\Fields\Field;
@@ -16,7 +17,15 @@ class LocationFormPage extends FormPage
      */
     public function fields(): array
     {
-        return [];
+        return [
+            Text::make('ID','id' ),
+            Text::make('Title', 'title'),
+            Text::make('Longitude', 'lng'),
+            Text::make('Latitude', 'lat'),
+            Text::make('Address', 'address'),
+            Text::make('City', 'city'),
+            Text::make('Postcode', 'postcode'),
+        ];
     }
 
     /**
--- app/MoonShine/Pages/Location/LocationIndexPage.php
+++ app/MoonShine/Pages/Location/LocationIndexPage.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Pages\Location;
 
+use MoonShine\Fields\Text;
 use MoonShine\Pages\Crud\IndexPage;
 use MoonShine\Components\MoonShineComponent;
 use MoonShine\Fields\Field;
@@ -16,7 +17,15 @@ class LocationIndexPage extends IndexPage
      */
     public function fields(): array
     {
-        return [];
+        return [
+            Text::make('ID','id' ),
+            Text::make('Title', 'title'),
+            Text::make('Longitude', 'lng'),
+            Text::make('Latitude', 'lat'),
+            Text::make('Address', 'address'),
+            Text::make('City', 'city'),
+            Text::make('Postcode', 'postcode'),
+        ];
     }
 
     /**
